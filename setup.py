9f4816f80a70b752d613395aba8e6bbb2c06fdf1
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2019-01-05 21:33:57 -0300

    fix folders in packaging and new version 0.9

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(
     name='fifbucket',
-    packages=find_packages(exclude=['venv*']),
-    version='0.8',
+    packages=['fifbucket'],
+    version='0.9',
     description='Bitbucket Api Library',
     author='Mario Faundez',
     author_email='mariofaundez@hotmail.com',
     url='https://github.com/fiftech/fifbucket',
-    download_url='https://github.com/fiftech/fifbucket/archive/0.8.tar.gz',
+    download_url='https://github.com/fiftech/fifbucket/archive/0.9.tar.gz',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
