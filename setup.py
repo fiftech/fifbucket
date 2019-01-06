8d69807471ca8b9d782439c1bbb5c50dcdc91f73
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2019-01-05 21:46:02 -0300

    add version as variable in new file version.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
+import os
 from setuptools import setup
 
+VERSION_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fifbucket', 'version.py')
+VERSION = None
+with open(VERSION_FILE, 'r') as f:
+    VERSION = f.read().split()[2][1:-1]
 with open('README.md') as f:
     long_description = f.read()
 
 setup(
     name='fifbucket',
     packages=['fifbucket'],
-    version='0.9',
+    version=VERSION,
     description='Bitbucket Api Library',
     author='Mario Faundez',
     author_email='mariofaundez@hotmail.com',
     url='https://github.com/fiftech/fifbucket',
-    download_url='https://github.com/fiftech/fifbucket/archive/0.9.tar.gz',
+    download_url='https://github.com/fiftech/fifbucket/archive/{}.tar.gz'.format(VERSION),
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
