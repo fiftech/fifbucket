95d3231068c54e677fee876e776fbd0f3d05ad0d
Mario Faúndez Vidal <mariofaundez@hotmail.com>
2019-04-26 16:03:41 -0400

    fix test version

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,6 +4,6 @@ import re
 
 def test_version():
     result = version.__version__
-    assert re.match(r'^\d+\.\d+$', result)
+    assert re.match(r'^\d+\.\d+(\.\d+)$', result)
     assert isinstance(result, str), 'wrong type!'
     assert not isinstance(result, int), 'wrong type!'
